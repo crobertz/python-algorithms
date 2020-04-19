a3ee2504268ae9e89da89e12ce1da716da967183
Ren Zhu <rockmanexe3@hotmail.com>
2020-04-19 19:01:31 -0400

    added simple test

--- murnaghan-nakayama/test_murnak.py
+++ murnaghan-nakayama/test_murnak.py
@@ -1,2 +1,12 @@
 import unittest
 import murnak
+
+
+class MurNakTest(unittest.TestCase):
+
+    def test_val(self):
+        self.assertEqual(murnak.character([1], [1]), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
