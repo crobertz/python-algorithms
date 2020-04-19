e0b3cb94dd1f86e8400cbffa5624d474742be85d
Ren Zhu <rockmanexe3@hotmail.com>
2020-04-19 19:17:52 -0400

    added test cases for charval function

--- murnaghan-nakayama/test_murnak.py
+++ murnaghan-nakayama/test_murnak.py
@@ -5,7 +5,16 @@ import murnak
 class MurNakTest(unittest.TestCase):
 
     def test_charval(self):
+        self.assertEqual(murnak.character([], []), 1)
         self.assertEqual(murnak.character([1], [1]), 1)
+        self.assertEqual(murnak.character([5, 2, 1], [3, 3, 1, 1]), -2)
+        self.assertEqual(murnak.character([3, 3, 1, 1], [5, 2, 1]), 1)
+        self.assertEqual(murnak.character([3, 2, 1], [1, 1, 1, 1, 1, 1]), 16)
+        self.assertEqual(murnak.character([3, 3, 3], [8, 1]), 0)
+        self.assertEqual(murnak.character([3, 3, 3], [4, 3, 2]), -2)
+        with self.assertRaises(SystemExit) as cm:
+            murnak.character([3, 2, 1], [2, 2])
+            self.assertEqual(cm.exception, "Need two partitions of same integer")
 
 
 if __name__ == '__main__':
