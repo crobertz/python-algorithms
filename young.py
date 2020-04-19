819d661274557e1059fcec79d6f80369b9c2a8e6
Ren Zhu <rockmanexe3@hotmail.com>
2020-04-18 23:10:28 -0400

    added function makeyoungiag which takes a partition and returns corresponding young diagram implemented as a directed graph

--- young.py
+++ young.py
@@ -14,11 +14,44 @@ def partitiontolist(partition):
 
     return return_list
 
+def makeyoungdiag(partition):
+    """
+    Given a partition, return corresponding Young diagram implemented as a graph
+    """
+    diagram = {}
+    parts_list = partitiontolist(partition)
+
+    #loop for each part in partition
+    for index in range(len(partition)):
+        part = parts_list[index]
+        part_length = partition[index]
+
+        next_part = []
+        if index + 1 < len(partition):
+            next_part = parts_list[index + 1]
+        part_length_next = len(next_part)
+
+        #if not the last element in part, add descendant
+        for k in range(part_length):
+            diagram[part[k]] = set()
+            #add descendant to right
+            if k + 1 < part_length:
+                diagram[part[k]].add(part[k+1])
+            #add descendant below
+            if k < part_length_next:
+                diagram[part[k]].add(next_part[k])
+
+    return diagram
+
 
 def main():
     print("Enter a parition in decreasing order:")
     partition = [int(n) for n in input().split()]
+    print("Partition:")
     print(partitiontolist(partition))
+    ydiag = makeyoungdiag(partition)
+    print("Corresponding Young diagram:")
+    print(ydiag)
 
 if __name__ == '__main__':
     main()
