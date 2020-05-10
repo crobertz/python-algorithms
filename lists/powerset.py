54399df350db9ffb44201405ba8978b584ce6a86
Ren Zhu <rockmanexe3@hotmail.com>
2020-05-10 17:58:18 -0400

    given n generates powerset of {0,1,...,n-1}

---
+++ lists/powerset.py
@@ -0,0 +1,35 @@
+def powerset(n):
+    """
+    Given positive integer n, returns powerset of {0,1,...,n-1}
+    """
+    # initialize with emptyset
+    subsets = [[]]
+    
+    # contains subsets from previous round
+    prev_subsets = []
+
+    # initialize with singletons
+    for i in range(n):
+        prev_subsets.append([i])
+    subsets.extend(prev_subsets)
+
+    # iterate over length of subset
+    for length in range(1,n):
+        # size k+1 subsets to be added
+        new_subsets = []
+
+        # at step k, prev_subsets = subsets of size k
+        # for each k-subset, add number ranging from largest element to n-1
+        for subset in prev_subsets:
+            last = subset[-1]
+            for num in range(last+1,n):
+                new_subset = subset + [num]
+                new_subsets.append(new_subset)
+        
+        # add newly found subset
+        subsets.extend(new_subsets)
+
+        # update new_subsets to be set of all size k+1 subsets
+        prev_subsets = new_subsets
+    
+    return subsets
