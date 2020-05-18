3841a92c33ccd9f34485ce3eb2b2cd7516e51262
Ren Zhu <rockmanexe3@hotmail.com>
2020-05-18 19:02:22 -0400

    renamed self.graph to self.adj to reflect adjacency data

--- graphs/graph.py
+++ graphs/graph.py
@@ -5,19 +5,19 @@ class Graph:
         # set of nodes
         self.nodes = set(node for node in nodes)
         # shows graph as a dict of node: adjacent nodes
-        self.graph = {node: set() for node in self.nodes}
+        self.adj = {node: set() for node in self.nodes}
         # set of tuples (source,target)
-        self.edges = set(tuple([node, nbr]) for node in self.nodes for nbr in self.graph[node])
+        self.edges = set(tuple([node, nbr]) for node in self.nodes for nbr in self.adj[node])
     
     def add_node(self, node):
         # add a node to graph
         self.nodes.add(node)
-        if not node in self.graph:
-            self.graph[node] = set()
+        if not node in self.adj:
+            self.adj[node] = set()
     
     def add_edge(self, source, target):
         # add an edge to graph
         self.add_node(source)
         self.add_node(target)
-        self.graph[source].add(target)
+        self.adj[source].add(target)
         self.edges.add(tuple([source,target]))
