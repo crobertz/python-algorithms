2cbbbbafc888ba2eb69ba4b7857ef04dd51d0bd8
Ren Zhu <rockmanexe3@hotmail.com>
2020-05-18 19:00:16 -0400

    implemented add_node and add_edge

--- graphs/graph.py
+++ graphs/graph.py
@@ -6,5 +6,18 @@ class Graph:
         self.nodes = set(node for node in nodes)
         # shows graph as a dict of node: adjacent nodes
         self.graph = {node: set() for node in self.nodes}
-        # set of tuples (origin,target)
+        # set of tuples (source,target)
         self.edges = set(tuple([node, nbr]) for node in self.nodes for nbr in self.graph[node])
+    
+    def add_node(self, node):
+        # add a node to graph
+        self.nodes.add(node)
+        if not node in self.graph:
+            self.graph[node] = set()
+    
+    def add_edge(self, source, target):
+        # add an edge to graph
+        self.add_node(source)
+        self.add_node(target)
+        self.graph[source].add(target)
+        self.edges.add(tuple([source,target]))
